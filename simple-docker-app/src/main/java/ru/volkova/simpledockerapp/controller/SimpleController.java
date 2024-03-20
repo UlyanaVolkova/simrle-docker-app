49f6ec057f2f7f594d5ffdb2c4ae7cbe608aadef
Ульяна <skipina_ulyana24@mail.ru>
2024-03-20 23:35:46 +0500

    Dockerfile and controller

---
+++ simple-docker-app/src/main/java/ru/volkova/simpledockerapp/controller/SimpleController.java
@@ -0,0 +1,12 @@
+package ru.volkova.simpledockerapp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SimpleController {
+    @GetMapping("/")
+    public String hello() {
+        return "Hello Docker!";
+    }
+}
